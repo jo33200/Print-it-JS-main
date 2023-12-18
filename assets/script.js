c7893b7fa1ca546e39f923b6be9e72a61b9468bc
jo33200 <jo33200@gmail.com>
2023-12-18 11:26:07 +0100

    build: 4 dots

--- assets/script.js
+++ assets/script.js
@@ -29,7 +29,30 @@ let parentElement = document.getElementById("banner");
 parentElement.appendChild(dotsContainer);
 dotsContainer.classList.add("dotsContainer");
 
+let dot = document.createElement("div");
+let parentElement2 = document.querySelector(".dotsContainer");
+parentElement2.appendChild(dot);
+dot.classList.add("dot");
+
+let dot2 = document.createElement("div");
+let parentElement3 = document.querySelector(".dotsContainer");
+parentElement3.appendChild(dot2);
+dot2.classList.add("dot");
+
+let dot3 = document.createElement("div");
+let parentElement4 = document.querySelector(".dotsContainer");
+parentElement4.appendChild(dot3);
+dot3.classList.add("dot");
+
+let dot4 = document.createElement("div");
+let parentElement5 = document.querySelector(".dotsContainer");
+parentElement5.appendChild(dot4);
+dot4.classList.add("dot");
+
+
 // Définir le img slider comme element du dom
+
+
 
 // Définir tagline
 
